dc42129ee873b96cd06e8134f2d0490fe1ecc8e8
MithunBurman7 <mithunburman7@gmail.com>
2017-06-12 15:24:18 +0530

    Initial commit

---
+++ src/bitwiseO/FirstBitwise.java
@@ -0,0 +1,21 @@
+package bitwiseO;
+
+import java.util.Scanner;
+
+public class FirstBitwise {
+
+	public static void main(String[] args) {
+	         Scanner in = new Scanner(System.in);
+	  
+             System.out.println("enter first number");
+             byte a = in.nextByte();
+        
+             System.out.println("enter first number");
+             byte b = in.nextByte();
+        
+             int x = a, y = b;
+             System.out.println(x==1 && y==1);
+	
+	}
+
+}
